31a6eabc8da3e19da8652c555b5478ed15edeef5
Daviqueiroz <daviqrozz2@gmail.com>
2023-06-20 12:56:24 -0300

    Deploy do sistema base nao estilizado

---
+++ core/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render,redirect
+from .models import Pessoa
+
+def home(request):
+    pessoas = Pessoa.objects.all()
+    return render(request,"index.html",{'pessoas':pessoas})
+
+
+def salvar(request):
+    nome = request.POST.get("nome")
+    idade = request.POST.get("idade")
+
+    Pessoa.objects.create(nome=nome, idade=idade)
+    pessoas = Pessoa.objects.all()
+
+    return render(request, 'index.html', {'pessoas': pessoas})
+
+def editar(request,id):
+    pessoa = Pessoa.objects.get(id=id)
+    return render(request,'update.html',{'pessoa':pessoa})
+
+def update(request,id):
+    nome = request.POST.get("nome")
+    idade = request.POST.get("idade")
+    pessoa = Pessoa.objects.get(id=id)
+    pessoa.nome = nome
+    pessoa.idade = idade
+    pessoa.save()
+    return redirect(home)
+
+def delete(request,id):
+    pessoa = Pessoa.objects.get(id=id)
+    pessoa.delete()
+    return redirect(home)
