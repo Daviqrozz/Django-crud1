c84937ec5b6e488ce0bf8ee332ad529cd212ce0a
Daviqueiroz <daviqrozz2@gmail.com>
2023-07-04 14:40:10 -0300

    Pagina de login adicionado

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home,salvar,editar,update,delete,cadastro
+from .views import home,salvar,editar,update,delete,cadastro,login_view
 
 
 urlpatterns = [
-path('',home),
+path('',login_view,name='login'),
+path('home/',home,name='home'),
 path('cadastro/',cadastro,name='cadastro'),
 path('salvar/',salvar,name='salvar'),
 path('editar/<int:id>',editar,name='editar'),
--- core/views.py
+++ core/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
 from .models import Pessoa
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = "Invalid username or password."
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        return render(request, 'login.html')
 
 def home(request):
     pessoas = Pessoa.objects.all()
