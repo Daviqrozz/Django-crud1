c7d031de76f6e63b4284c55fcb88606b0e6072cc
Daviqueiroz <daviqrozz2@gmail.com>
2023-07-06 18:43:29 -0300

    adicinado responsividade aos templates

--- core/models.py
+++ core/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Pessoa(models.Model):
     nome = models.CharField(max_length=100)
     idade = models.IntegerField()
+    
 
     def __str__(self):
         return self.nome
